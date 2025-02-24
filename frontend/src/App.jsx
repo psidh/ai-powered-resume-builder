3c5dd16ad5f80e6c4a98e4266598b7222340e4e2
P SIDHARTH <philkhanasidharth14@gmail.com>
2025-02-24 10:45:00 +0530

    front end boiler plate added
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+    hi there , this is your ai-powered resume builder
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Resume.jsx
@@ -0,0 +1,10 @@
+
+const Resume = () => {
+  return (
+    <div>
+    hi there this is resume page
+    </div>
+  )
+}
+
+export default Resume
